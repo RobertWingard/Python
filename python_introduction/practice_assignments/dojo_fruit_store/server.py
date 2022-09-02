2b32ebb6cb2597a0f6b7a54565acdb166b3373b7
RobertWingard <RobertWingard@Icloud.com>
2022-09-02 11:51:50 -0700

    checkout

--- python_introduction/practice_assignments/dojo_fruit_store/server.py
+++ python_introduction/practice_assignments/dojo_fruit_store/server.py
@@ -8,7 +8,14 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    cust = request.form['first_name']
+    last_name = request.form['last_name']
+    apple = request.form['apple']
+    strawberry = request.form['strawberry']
+    raspberry = request.form['raspberry']
+    count = int(apple)+int(strawberry)+int(raspberry)
+    print(f'hello there {cust} you have purchased {count} fruits')
+    return render_template("checkout.html", cust=cust, count=count, last_name=last_name, apple=apple,strawberry=strawberry,raspberry=raspberry)
 
 @app.route('/fruits')         
 def fruits():
