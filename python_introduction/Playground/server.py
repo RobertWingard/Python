48503e0345f8eab994f6de4c9251647da9cf43bf
RobertWingard <RobertWingard@Icloud.com>
2022-09-01 19:43:12 -0700

    squares

---
+++ python_introduction/Playground/server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("index.html", h=100, w=100, color='blue',)	# notice the 2 new named arguments!
+
+
+@app.route('/play')
+def play():
+    return render_template('index.html', h=100, w=100, color='blue')
+
+@app.route('/play/<int:number>/<color>')
+def multi(number, color):
+    return render_template('index.html', color=color, number=number) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
